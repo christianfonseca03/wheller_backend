bb2734a2d32cef34afeaee83296f3b3b7fdf3cb0
Christian Fonseca <chrisgfwheller@hotmail.com>
2023-10-09 19:51:09 -0300

    .

--- middleware.js
+++
@@ -1,8 +0,0 @@
-const corsOptions = {
-  origin: "*", // Substitua pelo domínio do seu frontend
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Habilita o envio de cookies de origem cruzada
-  optionsSuccessStatus: 204, // Define o código de status de resposta para preflight (opções) para 204
-};
-
-app.use(cors(corsOptions));
